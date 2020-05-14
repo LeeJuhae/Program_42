9df41f26c17489d9acd7898f3336e70485ba15ae
jjungeun <jungeun9729@gmail.com>
2020-05-15 01:09:15 +0900

    build error 수정

--- manage_db.py
+++ manage_db.py
@@ -4,7 +4,7 @@ from flask import Flask, request, redirect, render_template
 import os
 
 def connect_db():
-	engine = create_engine(os.environ['CLEARDB_DATABASE_URL'], encoding = 'utf-8', convert_unicode=False, ,pool_pre_ping=True, pool_size=20, pool_recycle=-1, max_overflow=20)
+	engine = create_engine(os.environ['CLEARDB_DATABASE_URL'], encoding = 'utf-8', convert_unicode=False, pool_pre_ping=True, pool_size=20, pool_recycle=-1, max_overflow=20)
 	# debug
 	# engine = create_engine(config.DB_URL, encoding = 'utf-8', convert_unicode=False, pool_size=20, pool_recycle=500, max_overflow=20)
 
